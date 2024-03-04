def401b65046ad944957eeda285477d39a2c660f
sohansatoru <160564688+sohansatoru@users.noreply.github.com>
2024-03-04 19:36:43 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+function setup() {
+    video = createCapture(VIDEO);
+    video.size(550,500);
+    
+    canvas = createCanvas(550,500);
+    canvas.position(560,150);
+    
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+    }
+    
+    function modelLoaded(){
+        console.log('poseNet Is Initialized')
+    }
+    
+    function text(){
+        background('#969A97');
+        fill('#F90093');
+        stroke('#F90093');
+        text('sohan', 20, 20)
+    }
+    
+    
+    function gotPoses(results){
+        noseX=0;
+        noseY=0;
+        if(results.length > 0)
+        {
+            console.log(results);
+            noseX = results[0].pose.nose.x;
+            noseY = results[0].pose.nose.y;
+            console.log("noseX =" + noseX +"noseY =" + noseY);
+            rightWristX = results[0].pose.rightWrist.x;
+            leftWristX = results[0].pose.leftWrist.x;
+            diffrence = floor( leftWristX - rightWristX);
+            console.log("leftWristX =" + leftWristX +"rightWristX =" + rightWristX + "diffrence =" +diffrence);
+        }
+    }
+    
+    
+    
